0bb14f9d5722cbddab62f9b88f68e8ec27e1fef9
Sami Fouad <sfouad@gmail.com>
2024-08-06 00:31:32 -0600

    gh workflow iteration

--- npm/gild/bin.js
+++ npm/gild/bin.js
@@ -2,45 +2,28 @@
 const { execSync } = require('child_process');
 const os = require('os');
 const fs = require('fs');
-const path = require('path');
 
-function findBinaryPath() {
-    // Determine if the script is being run globally or locally
-    const isGlobalInstall = path.dirname(require.main.filename).includes('node_modules');
-    let npmRoot;
+let binaryPath;
 
-    if (isGlobalInstall) {
-        // Global installation path
-        npmRoot = execSync('npm root -g').toString().trim();
-    } else {
-        // Local installation path
-        npmRoot = path.dirname(require.main.filename);
-    }
-
-    console.log(`npmRoot: ${npmRoot}`);
-
-    const binaryName = os.platform() === 'win32' ? 'gild.exe' : 'gild';
-    const binaryFolder = os.platform() === 'win32'
-        ? 'gild-windows-x64'
-        : os.platform() === 'linux'
-        ? 'gild-linux-x64'
-        : os.platform() === 'darwin'
-        ? os.arch() === 'x64'
-            ? 'gild-darwin-x64'
-            : 'gild-darwin-arm64'
-        : '';
-
-    const binaryPath = path.join(npmRoot, 'node_modules', '@samifouad', binaryFolder, binaryName);
-
-    console.log(`Binary Path: ${binaryPath}`);
-
-    if (fs.existsSync(binaryPath)) {
-        return binaryPath;
-    }
-
-    console.error(`Binary not found: ${binaryPath}`);
-    process.exit(1);
+switch (os.platform().toString().trim()) {
+    case 'darwin':
+        let dariwinNpmGlobal = execSync("npm root -g").toString().trim();
+        binaryPath = os.arch() === 'x64'
+            ? dariwinNpmGlobal +'/@samifouad/node_modules/@samifouad/rexds-darwin-x64/gild'
+            : dariwinNpmGlobal +'/@samifouad/node_modules/@samifouad/rexds-darwin-arm64/gild';
+        break;
+    case 'win32':
+        let winNpmGlobal = execSync("npm root -g").toString().trim();
+        binaryPath = winNpmGlobal +'\\@samifouad\\node_modules\\@samifouad\\rexds-windows-x64\\gild.exe';
+        break;
+    case 'linux':
+        let linuxNpmGlobal = execSync("npm root -g").toString().trim();
+        binaryPath = fs.realpathSync(linuxNpmGlobal +'/@samifouad/node_modules/@samifouad/rexds-linux-x64/gild');
+        break;
+    default:
+        console.error(`Unsupported platform: ${os.platform()}`);
+        process.exit(1);
 }
 
-const binaryPath = findBinaryPath();
+// Execute the binary with any arguments passed to the script
 execSync(`"${binaryPath}" ${process.argv.slice(2).join(' ')}`, { stdio: 'inherit' });
